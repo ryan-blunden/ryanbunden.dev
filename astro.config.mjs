5577b7c0b4a845e661d36cd8e01bd3a91e4bf66d
Ryan Blunden <ryan.blunden@gmail.com>
2024-01-30 21:58:01 +1000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ryanblunden.tech',
+  integrations: [starlight({    
+    title: 'Ryan Blunden',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/ryanblunden/',
+      github: 'https://github.com/ryan-blunden',
+      email: 'mailto:ryan.blunden@gmail.com',
+    },
+    customCss: ['./src/styles/tailwind.css', './src/styles/custom.css']
+  }), tailwind({
+    applyBaseStyles: false
+  })]
+});
