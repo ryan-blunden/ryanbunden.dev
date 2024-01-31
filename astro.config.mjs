eca91276b74806f724bbefa00d0a292d48eb5b3b
Ryan Blunden <ryan.blunden@gmail.com>
2024-01-31 13:10:43 +1000

    Added Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryanblunden.tech',
-  integrations: [starlight({    
-    title: 'Ryan Blunden',
-    social: {
-      linkedin: 'https://www.linkedin.com/in/ryanblunden/',
-      github: 'https://github.com/ryan-blunden',
-      email: 'mailto:ryan.blunden@gmail.com',
-    },
-    customCss: ['./src/styles/tailwind.css', './src/styles/custom.css']
-  }), tailwind({
-    applyBaseStyles: false
-  })]
+  integrations: [
+    starlight({
+      title: 'Ryan Blunden',
+      social: {
+        linkedin: 'https://www.linkedin.com/in/ryanblunden/',
+        github: 'https://github.com/ryan-blunden',
+        email: 'mailto:ryan.blunden@gmail.com',
+      },
+      customCss: ['./src/styles/tailwind.css', './src/styles/custom.css'],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
